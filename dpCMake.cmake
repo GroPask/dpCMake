06ea2273f3c408b93cd3c31c71cf851551c06a0b
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-04-09 13:44:53 +0200

    Remove new stuff from first-prototype branch

--- dpCMake.cmake
+++
@@ -1,8 +0,0 @@
-include_guard()
-
-include(${CMAKE_CURRENT_LIST_DIR}/Tools/GlobalTools.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/Tools/TargetTools.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/Tools/Warnings.cmake)
-
-include(${CMAKE_CURRENT_LIST_DIR}/Tools/ConfigureGlobal.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/Tools/ConfigureTarget.cmake)
