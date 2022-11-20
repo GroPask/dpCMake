0ab50bbe9c1abb345128d2ee2f044e267e2986c8
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-20 20:32:37 +0100

    Add dp_get_targets_list

--- Tools/MiscTools.cmake
+++ Tools/MiscTools.cmake
@@ -17,3 +17,31 @@ function (dp_replace_in_file filePath)
 
     file(WRITE ${filePath} ${fileContent})
 endfunction ()
+
+function (dp_get_targets_list outTargetsListVar)
+    set(options RECURSE)
+    set(oneValueArgs DIRECTORY)
+    set(multiValueArgs)
+    cmake_parse_arguments(DP_GET_TARGETS_LIST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  
+    if (DEFINED DP_GET_TARGETS_LIST_DIRECTORY)
+        set(dir ${DP_GET_TARGETS_LIST_DIRECTORY})
+    else ()
+        set(dir ${CMAKE_CURRENT_SOURCE_DIR})
+    endif ()
+
+    get_property(targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
+
+    if (DP_GET_TARGETS_LIST_RECURSE)
+        get_property(subDirs DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
+
+        foreach (subDir ${subDirs})
+            dp_get_targets_list(subDirTargets DIRECTORY ${subDir} RECURSE)
+            list(APPEND targets ${subDirTargets})
+        endforeach ()
+
+        list(REMOVE_DUPLICATES targets)
+    endif ()
+
+    set(${outTargetsListVar} ${targets} PARENT_SCOPE)
+endfunction ()
