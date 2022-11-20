89d8ebef29e7705b8ddf7fa8767a59e9b6eee2e9
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-20 20:39:09 +0100

    Make sure to include DependencyTools & MiscTools from main file

--- dpCMake.cmake
+++ dpCMake.cmake
@@ -1,6 +1,8 @@
 include_guard()
 
+include(${CMAKE_CURRENT_LIST_DIR}/Tools/DependencyTools.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Tools/GlobalTools.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Tools/MiscTools.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Tools/TargetTools.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Tools/Warnings.cmake)
 
