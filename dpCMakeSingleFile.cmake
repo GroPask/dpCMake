de2ed9bc8d7c97f9af2b6f89a180cc66317eba0d
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-22 19:25:45 +0100

    Make SingleFile use zip to download

--- dpCMakeSingleFile.cmake
+++ dpCMakeSingleFile.cmake
@@ -1,9 +1,6 @@
 include(FetchContent)
 
-FetchContent_Declare(dpCMake
-    GIT_REPOSITORY https://github.com/GroPask/dpCMake.git
-    GIT_TAG v0.0.3
-)
+FetchContent_Declare(dpCMake URL https://github.com/GroPask/dpCMake/archive/refs/tags/v0.0.3.zip)
 FetchContent_MakeAvailable(dpCMake)
 
 include(${dpcmake_SOURCE_DIR}/dpCMake.cmake)
