5e285b1d7d0e474654947e045e9b7d163e5c73d3
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-23 09:08:57 +0100

    Check dependency  is working in test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     const std::string s = fmt::format("The answer is {}.", 42);
     
-    if (s != "The answer is 42")
+    if (s != "The answer is 42.")
         return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
