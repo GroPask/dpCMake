37afefce2c358a4990baf376cb8d208b4aabd35c
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-23 09:07:02 +0100

    Try failing test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,15 @@
 #include <fmt/core.h>
 
+#include <string>
+
+#include <cstdlib>
+
 int main()
 {
-    fmt::print("Hello World !\n");
+    const std::string s = fmt::format("The answer is {}.", 42);
+    
+    if (s != "The answer is 42")
+        return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
 }
