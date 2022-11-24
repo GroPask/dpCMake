515a05e3780ac6bda879f0a15b5b4966df9467f2
Damien Pasqualinotto <93010861+GroPask@users.noreply.github.com>
2022-11-24 08:18:48 +0100

    Complete example with spdlog dependency

---
+++ example/cmake/Dependencies.cmake
@@ -0,0 +1,15 @@
+dp_download_and_add_dependency(URL https://github.com/fmtlib/fmt/archive/refs/tags/9.1.0.zip)
+
+function (patchSpdlog spdlogSrcDir)
+    dp_replace_in_file(${spdlogSrcDir}/CMakeLists.txt "set(CMAKE_BUILD_TYPE" "#set(CMAKE_BUILD_TYPE")
+endfunction ()
+
+function (configureSpdLog)
+    option(SPDLOG_FMT_EXTERNAL "" ON)
+endfunction ()
+
+dp_download_and_add_dependency(
+    URL https://github.com/gabime/spdlog/archive/refs/tags/v1.11.0.zip
+    PATCH_FUNC patchSpdlog
+    CONFIGURE_FUNC configureSpdLog
+)
--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,6 +1,9 @@
-#include <fmt/core.h>
+#pragma warning(push, 0)
+#pragma warning(disable: 4365)
+#include <spdlog/spdlog.h>
+#pragma warning(pop)
 
 int main()
 {
-    fmt::print("Hello World !\n");
+    spdlog::info("Hello {} !\n", "World");
 }
